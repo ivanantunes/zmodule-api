466a81a81db8cef47509da2b07a4e1922e84d703
Ivan Antunes <ivanantnes75@gmail.com>
2021-02-16 07:55:44 -0300

    correções e criado pasta de test

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Export File of the All Modules.
+ * @author Ivan Antunes <ivanantnes75@gmail.com>
+ * @copyright Ivan Antunes 2021
+ */
+
+export * from './configs';
+// export * from './controllers';
+export * from './enums';
+export * from './interfaces';
+export * from './locale';
+export * from './services';
